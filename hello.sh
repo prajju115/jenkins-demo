bae3015547a2fa1a8d4adba3dc1cbd4b5e0ef512
Cloud User <ec2-user@ip-172-31-38-125.ap-south-1.compute.internal>
2026-07-07 07:13:44 +0000

    Added hello.sh for Jenkins build

---
+++ hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "======================"
+echo "Welcome to Jenkins"
+echo "This is my first Jenkins Project"
+echo "running on Amazon EC2"
+echo "Build Successful"
+echo "======================"
